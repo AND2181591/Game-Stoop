773e7aa7ff7225bd44f72dd6b6f73d5e364f739b
Andrew <AND2181591@maricopa.edu>
2021-10-15 15:11:58 -0700

    Test commit for ignoring environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true, 
   BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com', 
-  API_KEY: '37c7d4eb971a42cb8f5512e29209ec62'
+  API_KEY: 'df2332c71a014b7f8fa4403b2afe00bb'
 };
