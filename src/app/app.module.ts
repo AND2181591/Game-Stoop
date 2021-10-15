c640b1181e588f632ade97fb8c5ceb8bd9f93a74
Andrew <AND2181591@maricopa.edu>
2021-10-14 21:03:00 -0700

    Initial commit for the Game Stoop website. Starting off styling the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
