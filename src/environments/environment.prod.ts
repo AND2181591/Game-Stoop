173bc6cc3ca79a2666fea622ba9e6f76c76ef39c
Andrew <AND2181591@maricopa.edu>
2021-11-22 21:19:46 -0700

    Corrected the base url in the environments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true, 
-  BASE_URL: 'https://whatoplay.p.rapidapi.com/platform', 
+  BASE_URL: 'https://whatoplay.p.rapidapi.com/', 
   API_KEY: 'f1d3889813msh85cf14bda58d596p1c7133jsn950166e0f269'
 };
