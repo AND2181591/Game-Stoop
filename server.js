02ca27351bd23c91bb1bdb82df65f8a26d8e8960
Andrew <AND2181591@maricopa.edu>
2021-11-22 21:09:36 -0700

    Reverted back to previous package.json configurations

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/game-stoop'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/game-stoop/index.html'));});
-app.listen(process.env.PORT || 8080);
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/game-stoop'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/game-stoop/index.html'));});
+// app.listen(process.env.PORT || 8080);
